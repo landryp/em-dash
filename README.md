3ccbc73168f4c26933f26856dced2145444da9b5
Philippe Landry <philippe.landry@jupyter4.ligo.caltech.edu>
2024-05-15 21:38:43 -0700

    add bns notebook

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ##### em-dash
-### interactive plot of remnant matter predictions for compact binary mergers
+### visualization of remnant matter predictions for compact binary mergers
 philippe landry (pgjlandry@gmail.com) 04/2024
 
 *is a given compact binary merger candidate event predicted to have enough remnant matter to power an electromagnetic counterpart or not?*
@@ -10,10 +10,13 @@ philippe landry (pgjlandry@gmail.com) 04/2024
 
 nsbh-remnant.ipynb *# plot remnant matter contours from Foucart+ 2018 fit to NSBH merger simulations and compare against estimated event parameters*
 
+bns-remnant.ipynb *# plot remnant matter contours from Kruger+Foucart 2020 fit to BNS merger simulations and compare against estimated event parameters*
+
 ### Data
 
-*the etc/ directory contains the data and utilities needed for the analysis*
+*the src/ directory contains the utilities needed for the analysis*
 
-### Scripts
-
-*these scripts are used for production analyses*
+fhn18.py # Foucart+ 2018 fit for remnant matter in NSBH merger simulations \
+kf20.py # Kruger+Foucart 2020 fit for remnant matter in NSBH merger simulations \
+plot.py # main plotting utility \
+utils.py # define physical constants and other useful quantities
--- src/plot.py
+++ src/plot.py
@@ -3,6 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from src.fhn18 import F_remnant
+from src.kf20 import F_remnant as bns_F_remnant
 
 # contour plot
 
@@ -34,3 +35,34 @@ def remnant_mass_contour_plot(mBHgrid,mNSgrid,chiBHs,RNS,F_threshold,mBHrange=No
     plt.ylim(max(mBHgrid[0],mBHrange[0]),min(mBHgrid[1],mBHrange[1]))
     plt.xlabel(r'$m_{NS}\;[M_\odot]$',fontsize=16)
     plt.ylabel(r'$m_{BH}\;[M_\odot]$',fontsize=16)
+
+def bns_remnant_mass_contour_plot(mNSgrid,RNS,F_threshold,m1range=None,m2range=None,qrange=None,model=bns_F_remnant,ngrid=100,shade=0.05,fade=0.5):
+
+    ms = np.linspace(mNSgrid[0],mNSgrid[1],ngrid)
+    
+    m1s,m2s = np.meshgrid(ms,ms)
+    
+    for F_thresh in np.linspace(0.01,F_threshold,3):
+    
+        Fs = model(m1s,m2s,RNS)
+    
+        cs = plt.contour(m2s,m1s,Fs,[F_thresh,1.],colors='k',linewidths=0.5,zorder=-1)
+        plt.contourf(m2s,m1s,Fs,[F_thresh,1.],colors='k',alpha=shade,zorder=-3)
+    
+        plt.clabel(cs, cs.levels, inline=True, fmt={c: '{0:.2f}'.format(F_thresh) for c in cs.levels}, fontsize=10,zorder=-1)
+
+    if m1range is None: m1range = mNSgrid
+    if m2range is None: m2range = mNSgrid
+    if qrange is None: qrange = (0.4,1.)
+
+    plt.fill_between(ms,max(mNSgrid[1],m1range[1]),ms/qrange[0],facecolor='white',edgecolor=None,alpha=fade,zorder=-2)
+    plt.fill_between(ms,min(mNSgrid[0],m1range[0]),ms/qrange[1],facecolor='white',edgecolor=None,alpha=fade,zorder=-2)
+    plt.fill_between(ms[ms < m2range[0]],ms[ms < m2range[0]]/qrange[0],ms[ms < m2range[0]]/qrange[1],facecolor='white',edgecolor=None,alpha=fade,zorder=-2)
+    plt.fill_between(ms[ms > m2range[1]],ms[ms > m2range[1]]/qrange[0],ms[ms > m2range[1]]/qrange[1],facecolor='white',edgecolor=None,alpha=fade,zorder=-2)
+
+    plt.fill_between(ms,ms,min(mNSgrid[0],m1range[0]),facecolor='white',edgecolor='k',alpha=1,lw=0.5,zorder=10)
+
+    plt.xlim(mNSgrid[0],mNSgrid[1])
+    plt.ylim(mNSgrid[0],mNSgrid[1])
+    plt.xlabel(r'$m_2\;[M_\odot]$',fontsize=16)
+    plt.ylabel(r'$m_1\;[M_\odot]$',fontsize=16)
